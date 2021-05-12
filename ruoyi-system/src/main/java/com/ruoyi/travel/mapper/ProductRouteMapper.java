1474e0b5864f58edbacc072f9d32a99d9333fd17
StephenZhu1120 <stephenzhu@buaa.edu.cn>
2021-05-12 11:21:42 +0800

    product_basic表添加了mapper的自定义方法

--- ruoyi-system/src/main/java/com/ruoyi/travel/mapper/ProductRouteMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/travel/mapper/ProductRouteMapper.java
@@ -5,15 +5,15 @@ import com.ruoyi.travel.domain.ProductRoute;
 
 /**
  * 产品路线Mapper接口
- * 
+ *
  * @author buaa_travel
  * @date 2021-05-11
  */
-public interface ProductRouteMapper 
+public interface ProductRouteMapper
 {
     /**
      * 查询产品路线
-     * 
+     *
      * @param id 产品路线ID
      * @return 产品路线
      */
@@ -29,15 +29,23 @@ public interface ProductRouteMapper
 
     /**
      * 查询产品路线列表
-     * 
+     *
      * @param productRoute 产品路线
      * @return 产品路线集合
      */
     public List<ProductRoute> selectProductRouteList(ProductRoute productRoute);
 
     /**
+     * 查询产品所有路线的价格最小值
+     *
+     * @param productId 产品id
+     * @return 价格最小值
+     */
+    public int getProductRoutePriceMin(Long productId);
+
+    /**
      * 新增产品路线
-     * 
+     *
      * @param productRoute 产品路线
      * @return 结果
      */
@@ -45,7 +53,7 @@ public interface ProductRouteMapper
 
     /**
      * 修改产品路线
-     * 
+     *
      * @param productRoute 产品路线
      * @return 结果
      */
@@ -53,7 +61,7 @@ public interface ProductRouteMapper
 
     /**
      * 删除产品路线
-     * 
+     *
      * @param id 产品路线ID
      * @return 结果
      */
@@ -61,7 +69,7 @@ public interface ProductRouteMapper
 
     /**
      * 批量删除产品路线
-     * 
+     *
      * @param ids 需要删除的数据ID
      * @return 结果
      */
