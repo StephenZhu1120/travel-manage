637268a105c73bf1f2938a99fbe2cbb6f49701bc
StephenZhu1120 <stephenzhu@buaa.edu.cn>
2021-05-20 22:07:18 +0800

    查看产品detail信息接口 fix完成

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/app/ProductMallController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/app/ProductMallController.java
@@ -1,5 +1,7 @@
 package com.ruoyi.web.controller.app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.ruoyi.common.core.controller.BaseController;
@@ -45,8 +47,29 @@ public class ProductMallController extends BaseController{
     @GetMapping("/getProduct")
     public AjaxResult getProduct(@RequestBody ProductBasic productBasic)
     {
-        Product_Route_Img product_route_img;
-        return AjaxResult.success(productBasicService.selectProductBasicById(productBasic.getId()));
+        Product_Route_Img product_route_img = new Product_Route_Img();//新建一个前端商城信息展示类
+
+        ProductRoute productRoute = new ProductRoute();//新建一个路线查询
+        productRoute.setProductId(productBasic.getId());
+        ProductBasic new_productBasic = productBasicService.selectProductBasicById(productBasic.getId());//通过id查询到产品
+        List<ProductRoute> new_productRoute = productRouteService.selectProductRouteList(productRoute);//通过id查询到路线的List
+
+        product_route_img.setId(new_productBasic.getId()); product_route_img.setProductName(new_productBasic.getProductName());
+        product_route_img.setProductStatus(new_productBasic.getProductStatus()); product_route_img.setProductDay(new_productBasic.getProductDay());
+        product_route_img.setBirthland(new_productBasic.getBirthland()); product_route_img.setDestination(new_productBasic.getDestination());
+        product_route_img.setPriceStart(new_productBasic.getPriceStart()); product_route_img.setOrderNumber(new_productBasic.getOrderNumber());
+        product_route_img.setDescription(new_productBasic.getDescription());product_route_img.setProductDetailAds(new_productBasic.getProductDetailAds());
+        if(!(new_productBasic.getImgUrl() == null || new_productBasic.getImgUrl().equals("")))
+            product_route_img.setImgs(Arrays.asList(new_productBasic.getImgUrl().split(";")));//按照分号对图片地址进行分割
+        if(new_productRoute.size() != 0) {
+            for (int i = 0; i < new_productRoute.size(); i++) {//对路线详细报价进行隐藏屏蔽，不显示给客户
+                new_productRoute.get(i).setHotelPrice(null);
+                new_productRoute.get(i).setTransportPrice(null);
+                new_productRoute.get(i).setTravelType(null);
+            }
+            product_route_img.setProductRoutes(new_productRoute);
+        }
+        return AjaxResult.success(product_route_img);
     }
 
     /**
